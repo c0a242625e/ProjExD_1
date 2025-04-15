3ecdeb4832a194c80ee7bb50efca463121b5c84b
c0a242625e <c0a242625e@edu.teu.ac.jp>
2025-04-15 17:07:15 +0900

    課題１：勝手に動く・右にも行く

--- pygame_image.py
+++ pygame_image.py
@@ -21,6 +21,7 @@ def main():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed() #練習10-3
 
+        kk_rct.move_ip(-1,0)
         if key_lst[pg.K_UP]:
             kk_rct.move_ip(0,-1) #練習10-4
         if key_lst[pg.K_DOWN]:
@@ -28,7 +29,7 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip(-1,0)
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(1,0)
+            kk_rct.move_ip(2,0)
         x = tmr%3200
         screen.blit(bg_img, [-x, 0]) #練習6
         screen.blit(bg_img2,[-x+1600,0]) #練習7
